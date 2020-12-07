96dc1b452ff10f878a7d58a09cae3a3bc4d58baa
Matt Cushing <matt.cushing@eclypses.com>
2020-12-06 22:20:01 -0700

    All changes, sorry that I did not make frequent commits but I did not think I would need to upload this for help

---
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -0,0 +1,33 @@
+package com.example.android.navigation
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.android.navigation.databinding.FragmentTitleBinding
+
+class TitleFragment : Fragment() {
+    private lateinit var binding: FragmentTitleBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentTitleBinding.inflate(inflater, container, false)
+
+        binding.playButton.setOnClickListener(
+                            Navigation.createNavigateOnClickListener(TitleFragmentDirections.actionTitleFragmentToGameFragment().actionId))
+        setHasOptionsMenu(true)
+
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.overflow_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, binding.root.findNavController())
+                || super.onOptionsItemSelected(item)
+    }
+}
